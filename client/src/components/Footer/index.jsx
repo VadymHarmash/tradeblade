2a42765741d41b67b68926274d53a1c7d70fbefe
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2025-03-10 16:27:24 +0200

    added start setup

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles/footer.module.scss";
+
+export const Footer = () => {
+  return (
+    <div>
+      <div className="container"></div>
+    </div>
+  );
+};
---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles/header.module.scss";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className="container"></div>
+    </div>
+  );
+};
